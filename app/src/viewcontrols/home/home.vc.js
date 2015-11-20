9eac75a3dd2d893005b1732a2aa02bce9e74aba4
Walter Moore <walteragnewmoore@gmail.com>
2015-11-19 21:40:49 -0600

    transfer of angular code, yet to modify

--- app/src/viewcontrols/home/home.vc.js
+++ app/src/viewcontrols/home/home.vc.js
@@ -17,3 +17,43 @@ var HomeViewControl = (function (_super) {
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = HomeViewControl;
 platypus_1.register.viewControl('home-vc', HomeViewControl);
+(function () {
+    var app = angular.module('myApp', []);
+    app.controller('CalcController', ['$scope', '$window', function ($scope, $window) {
+            $scope.title = "The Calculator";
+            $scope.result = null;
+            $scope.input1 = null;
+            $scope.input2 = null;
+            $scope.operation = function (operator) {
+                if ($scope.input1 == null || $scope.input2 == null) {
+                    alert("Both operands are required");
+                    return;
+                }
+                else if (operator == "+") {
+                    $scope.result = $scope.input1 + $scope.input2;
+                    $scope.para = "The result of adding " + $scope.input1 + " and " + $scope.input2 + " is " + $scope.result;
+                }
+                else if (operator == "-") {
+                    $scope.result = $scope.input1 - $scope.input2;
+                    $scope.para = "The result of subtracting " + $scope.input1 + " and " + $scope.input2 + " is " + $scope.result;
+                }
+                else if (operator == "*") {
+                    $scope.result = $scope.input1 * $scope.input2;
+                    $scope.para = "The result of multiplying " + $scope.input1 + " and " + $scope.input2 + " is " + $scope.result;
+                }
+                else if (operator == "/") {
+                    if ($scope.input2 == 0) {
+                        alert("Are you trying to kill us all? Please don't divide by zero");
+                        return;
+                    }
+                    else {
+                        $scope.result = $scope.input1 / $scope.input2;
+                        $scope.para = "The result of dividing " + $scope.input1 + " and " + $scope.input2 + " is " + $scope.result;
+                    }
+                    ;
+                }
+                $scope.input1 = 0;
+                $scope.input2 = 0;
+            };
+        }]);
+})();
--- app/src/viewcontrols/home/home.vc.ts
+++ app/src/viewcontrols/home/home.vc.ts
@@ -8,3 +8,45 @@ export default class HomeViewControl extends BaseViewControl {
 }
 
 register.viewControl('home-vc', HomeViewControl);
+
+//Angular code to be turned into Platypi:
+
+(function(){
+
+var app = angular.module('myApp',[]);
+	
+app.controller('CalcController', ['$scope', '$window', function ($scope, $window){
+	
+	$scope.title="The Calculator";
+	$scope.result=null;
+	$scope.input1=null;
+	$scope.input2=null;
+	
+		$scope.operation = function(operator){
+			if ($scope.input1 == null || $scope.input2 == null){
+				alert("Both operands are required");
+				return;
+			}else if(operator == "+"){
+				$scope.result = $scope.input1 + $scope.input2;
+				$scope.para = "The result of adding " + $scope.input1 + " and " + $scope.input2 + " is " + $scope.result;
+			}else if(operator == "-"){
+				$scope.result = $scope.input1 - $scope.input2;
+				$scope.para = "The result of subtracting " + $scope.input1 + " and " + $scope.input2 + " is " + $scope.result;
+			}else if(operator == "*"){
+				$scope.result = $scope.input1 * $scope.input2;
+				$scope.para = "The result of multiplying " + $scope.input1 + " and " + $scope.input2 + " is " + $scope.result;
+			}else if(operator == "/"){
+				if($scope.input2 == 0){
+				alert("Are you trying to kill us all? Please don't divide by zero");
+				return;
+				}else{
+				$scope.result = $scope.input1 / $scope.input2;
+				$scope.para = "The result of dividing " + $scope.input1 + " and " + $scope.input2 + " is " + $scope.result;
+				};
+			}
+			$scope.input1 = 0;
+			$scope.input2 = 0;	
+		};
+		
+}]);
+})();
