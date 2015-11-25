f81de6f907980d579a5ff04bea307a5aebd22c0f
Walter Moore <walteragnewmoore@gmail.com>
2015-11-25 10:44:14 -0600

    some clean-up

--- app/src/viewcontrols/home/home.vc.ts
+++ app/src/viewcontrols/home/home.vc.ts
@@ -1,45 +1,48 @@
-import {register} from 'platypus';
+import {
+    register
+}
+from 'platypus';
 import BaseViewControl from '../base/base.vc';
 
 export default class HomeViewControl extends BaseViewControl {
     templateString: string = require('./home.vc.html');
 
     context: contexts.IHomeContext = {
-		title: "A Platypi Calculator",
-		input1: null,
-		input2: null,
-		para: ''
+        title: "A Platypi Calculator",
+        input1: null,
+        input2: null,
+        para: ''
 
-	};
-	
-	operation(operator: string){
-		var result: number;
-		var op1: number = this.context.input1;
-		var op2: number = this.context.input2;
-		if(this.utils.isEmpty(this.context.input1) || this.utils.isEmpty(this.context.input2)){
-			alert("Both operands are required!");
-			return;
-		}else if(operator == "+"){
-			result = op1 + op2;
-			this.context.para = "The result of adding " + op1 + " and " + op2 + " is " + result;
-		}else if(operator == "-"){
-			result = op1 - op2;
-			this.context.para = "The result of subtracting " + op1 + " and " + op2 + " is " + result;
-		}else if(operator == "*"){
-			result = op1 * op2;
-			this.context.para = "The result of multiplying " + op1 + " and " + op2 + " is " + result;			
-		}else if(operator =="/" && op2 == 0){
-			alert("Are you trying to kill us all? Please don't divide by zero");
-			return;
-			}else{
-			result = op1 / op2;
-			this.context.para = "The result of dividing " + op1 + " and " + op2 + " is " + result;
-		}
-		this.context.input1 = 0;
-		this.context.input2 = 0;	
-		
-	}
-	
+    };
+
+    operation(operator: string) {
+        var result: number;
+        var op1: number = this.context.input1;
+        var op2: number = this.context.input2;
+        if (this.utils.isEmpty(this.context.input1) || this.utils.isEmpty(this.context.input2)) {
+            alert("Both operands are required!");
+            return;
+        } else if (operator == "+") {
+            result = op1 + op2;
+            this.context.para = "The result of adding " + op1 + " and " + op2 + " is " + result;
+        } else if (operator == "-") {
+            result = op1 - op2;
+            this.context.para = "The result of subtracting " + op1 + " and " + op2 + " is " + result;
+        } else if (operator == "*") {
+            result = op1 * op2;
+            this.context.para = "The result of multiplying " + op1 + " and " + op2 + " is " + result;
+        } else if (operator == "/" && op2 == 0) {
+            alert("Are you trying to kill us all? Please don't divide by zero");
+            return;
+        } else {
+            result = op1 / op2;
+            this.context.para = "The result of dividing " + op1 + " and " + op2 + " is " + result;
+        }
+        this.context.input1 = 0;
+        this.context.input2 = 0;
+
+    }
+
 }
 
 register.viewControl('home-vc', HomeViewControl);
